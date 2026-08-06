2afb0200f1692496eaf1ba5c094cb50dbff6a851
Oblykode <aniltmg871@gmail.com>
2026-08-06 11:57:25 +0545

    removing gtx

--- supabase/functions/translate/index.ts
+++ supabase/functions/translate/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'npm:@supabase/supabase-js@2'
 import { corsHeaders, jsonOk, jsonError, handleError } from '../_shared/response.ts'
 import { getTranslationProvider } from '../_shared/translation-provider.ts'
 import {
